6dc1268fd965c2f21882792f92401f82cc0b5ea1
Bimal Mallick <ghostridergothan8460@gmail.com>
2025-09-22 10:27:20 +0530

    Word_counter

---
+++ word_frequency_counter.py
@@ -0,0 +1,10 @@
+#TODO:  Accept the user input and clean the input
+class CleanInput():
+    def __init__(self) -> None:
+        pass
+
+    def take_input(self):
+        pass
+
+    def clean_user(self):
+        pass
